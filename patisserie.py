d63fe8b05cd9527d143138b5bc45125f86e9486e
matthieu-27 <matthieu27@pm.me>
2025-12-04 15:52:27 +0100

    feat: Added Appareil and Recipient classes
    - Appareil: name attribute to identify appareil
    - Appareil: ingredients list that stock ingredients
    - Appareil: ajouter_ingredient to add ingredients to the list
    
    - Recipient: name attribute to identify recipient
    - Recipient : contenu attribute for stocking an Ingredient or an Appareil
    
    - Modified BatteurOeufs and FondeurChocolat to accept a recipient object
    - Updated run methods

--- patisserie.py
+++ patisserie.py
@@ -31,62 +31,64 @@ class Chocolat(Ingredient):
         super().__init__("Chocolat", quantity, "grammes")
 
 
-@dataclass
 class Appareil:
-    name: str
-
     def __init__(self, name):
         self.name = name
         self.ingredients = []
-
-    def __str__(self):
-        print(f"Je lance la préparation de {self.name}")
 
     def ajouter_ingredient(self, ingredient):
         self.ingredients.append(ingredient)
 
 
+class Recipient:
+    def __init__(self, name:str, contenu=None):
+        self.name = name
+        self.contenu = contenu
+
+
 class BatteurOeufs(Commis, threading.Thread):
-    def __init__(self, name, nb_oeufs):
+    def __init__(self, name: str, recipient: Recipient):
         threading.Thread.__init__(self)
         Commis.__init__(self, name)
-        self.nb_oeufs = nb_oeufs
+        self.recipient = recipient
 
     def run(self):
-        nb_tours = self.nb_oeufs * 8
-        for no_tour in range(1, nb_tours + 1):
-            print(f"{self.name}: Je bats les {self.nb_oeufs} oeufs, tour n°{no_tour}")
-            time.sleep(0.5)
+        if isinstance(self.recipient.contenu, Oeuf):
+            nb_oeufs = self.recipient.contenu.quantity
+            nb_tours = nb_oeufs * 8
+            for num_tour in range(1, nb_tours + 1):
+                print(f"{self.name}: Je bats les {nb_oeufs} oeufs dans : {self.recipient.name}, tour n°{num_tour}")
+                time.sleep(0.5)
 
 
 class FondeurChocolat(Commis, threading.Thread):
-    def __init__(self, name, quantite):
+    def __init__(self, name: str, recipient: Recipient):
         threading.Thread.__init__(self)
         Commis.__init__(self, name)
-        self.quantite = quantite
+        self.recipient = recipient
 
     def run(self):
-        print(f"{self.name}: Je mets de l'eau à chauffer dans une bouilloire")
-        time.sleep(8)
-        print(f"{self.name}: Je verse l'eau dans une casserole")
-        time.sleep(2)
-        print(f"{self.name}: J'y pose le bol rempli de chocolat")
-        time.sleep(1)
-        nb_tours = math.ceil(self.quantite / 10)
-        for no_tour in range(1, nb_tours + 1):
-            print(f"{self.name}: Je mélange {self.quantite} de chocolat à fondre, tour n°{no_tour}")
+        if isinstance(self.recipient.contenu, Chocolat):
+            quantity = self.recipient.contenu.quantity
+            print(f"{self.name}: Je mets de l'eau à chauffer dans une bouilloire")
+            time.sleep(8)
+            print(f"{self.name}: Je verse l'eau dans une casserole")
+            time.sleep(2)
+            print(f"{self.name}: J'y pose le {self.recipient.name} rempli de chocolat")
             time.sleep(1)
+            nb_tours = math.ceil(quantity / 10)
+            for num_tour in range(1, nb_tours + 1):
+                print(f"{self.name}: Je mélange {quantity} de chocolat à fondre dans : {self.recipient.name}, tour n°{num_tour}")
+                time.sleep(1)
 
 
 if __name__ == "__main__":
-    # Exemple d'utilisation de la classe Appareil
-    appareil = Appareil("Pâte à gâteau")
-    appareil.__str__()
-    appareil.ajouter_ingredient(Oeuf(5))
-    appareil.ajouter_ingredient(Chocolat(200))
+    # Exemple d'utilisation des classes
+    cul_de_poule = Recipient("cul de poule", Oeuf(5))
+    casserole = Recipient("casserole", Chocolat(200))
 
-    batteur = BatteurOeufs("Batteur", 5)
-    fondeur = FondeurChocolat("Fondeur", 200)
+    batteur = BatteurOeufs("Batteur", cul_de_poule)
+    fondeur = FondeurChocolat("Fondeur", casserole)
     batteur.start()
     fondeur.start()
     batteur.join()
